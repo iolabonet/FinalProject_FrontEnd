28a3a001ccbc5855994ea3020fb8c9a350429987
iolabonet <iolabonet.atg@gmail.com>
2023-04-15 14:32:53 +0200

    First commit

---
+++ src/components/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script setup>
+// import { create } from 'domain';
+// import { supabase } from './supabase/index';
+
+// async create (); {
+//     const { data: { user } } = await supabase.auth.getUser()
+//     console.log(user)
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <HomeView />
+    <h1>Final Project from iola</h1>
+  </main>
+</template>
+
+<script setup>
+import HomeView from '../components/HomeView.vue'
+
+</script>
+
+
+<style scoped>
+</style>
